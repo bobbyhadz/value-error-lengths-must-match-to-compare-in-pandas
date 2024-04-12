0dfebe1a7379bf0b97e540c951023f790eb2699d
bobbyhadz <opmftw@gmail.com>
2024-04-12 09:38:30 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Pandas ValueError: ('Lengths must match to compare')
+
+import pandas as pd
+
+df = pd.DataFrame({
+    'numbers': [1, 2, 3],
+    'date': ['2023-01-05', '2023-03-25', '2023-01-24']
+})
+
+arr = ['2023-01-05']
+
+print(df.iloc[0]['date'])  # 👉️ "2023-01-05"
+
+if df.iloc[0]['date'] == arr[0]:
+    # 👇️ this runs
+    print('success')
+else:
+    print('failure')
